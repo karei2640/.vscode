800c739d12459c57f3ff89037c7f0c62c0d799a7
karei2640 <karei2640@yahoo.co.jp>
2023-05-22 23:33:12 +0900

    様々な計算式

---
+++ AtCoder.practice/calculation.rb
@@ -0,0 +1,34 @@
+result = 5 + 3  # 加算
+puts result    # 結果: 8
+
+result = 10 - 2  # 減算
+puts result     # 結果: 8
+
+result = 4 * 6  # 乗算
+puts result    # 結果: 24
+
+result = 20 / 5  # 除算
+puts result     # 結果: 4
+
+result = 2 ** 3  # 2の3乗
+puts result      # 結果: 8
+
+result = 10 ** 2  # 10の2乗
+puts result      # 結果: 100
+
+result = 10 / 3
+puts result  # 結果: 3
+
+result = 10.0 / 3.0
+puts result  # 結果: 3.3333333333333335
+
+quotient, remainder = 10.divmod(3)
+puts quotient   # 結果: 3
+puts remainder  # 結果: 1
+
+result = 15 % 4  # 15を4で割った余り
+puts result     # 結果: 3
+
+result = 7 % 2   # 7を2で割った余り
+puts result     # 結果: 1
+
